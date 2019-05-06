988fab4fa99b64768e765073a39decdb0d8e6182
Jerome Grubb <49626010+jfg50@users.noreply.github.com>
2019-05-07 01:54:59 +1200

    Update MemberSelection.java

--- src/game/MemberSelection.java
+++ src/game/MemberSelection.java
@@ -8,6 +8,7 @@ import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -59,39 +60,43 @@ public class MemberSelection extends JFrame {
 		frame.setBounds(100, 100, 703, 564);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton(game.getCrew1().getCrewMemberList().get(0).getName());
-		btnNewButton.setBounds(21, 338, 147, 58);
-		frame.getContentPane().add(btnNewButton);
-
-
-		
-
-		
-		JLabel label = new JLabel("<html>i<br>i<br>i<br>i<br>i<br>i<br>i<br>");
+		JLabel label = new JLabel("");
 		label.setForeground(Color.BLACK);
 		label.setBackground(Color.WHITE);
 		label.setFont(new Font("Dialog", Font.PLAIN, 15));
 		label.setBounds(21, 140, 158, 142);
 		frame.getContentPane().add(label);
-		if (game.getCrew1().getCrewMemberList().size() >= 1) {
+		if (game.getCrew1().getCrewMemberList().size() >= 2) {
 			CrewMember member1 = game.getCrew1().getCrewMemberList().get(0);
-			label.setText(game.convertToMultiline(game.getCrew1().getCrewMemberList().get(0).viewStatus()));
+			label.setText(game.convertToMultiline(member1.viewStatus()));
+			JToggleButton btnNewButton = new JToggleButton(game.getCrew1().getCrewMemberList().get(0).getName());
+			btnNewButton.setBounds(21, 338, 147, 58);
+			frame.getContentPane().add(btnNewButton);
+			btnNewButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					game.setPickedMember(member1);
+				}
+			});
 		}
 		
-		JLabel label_1 = new JLabel("<html>i<br>i<br>i<br>i<br>i<br>i<br>i<br>");
+		JLabel label_1 = new JLabel("");
 		label_1.setFont(new Font("Dialog", Font.PLAIN, 15));
 		label_1.setBounds(188, 140, 155, 142);
 		frame.getContentPane().add(label_1);
 		if (game.getCrew1().getCrewMemberList().size() >= 2) {
 			CrewMember member2 = game.getCrew1().getCrewMemberList().get(1);
 			label_1.setText(game.convertToMultiline(member2.viewStatus()));
-			JButton btnMember = new JButton(member2.getName());
-			btnMember.setBounds(188, 338, 147, 58);
-			frame.getContentPane().add(btnMember);
-
+			JToggleButton btnMember20 = new JToggleButton(member2.getName());
+			btnMember20.setBounds(188, 338, 147, 58);
+			frame.getContentPane().add(btnMember20);
+			btnMember20.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					game.setPickedMember(member2);
+				}
+			});
 		}
 		
-		JLabel label_2 = new JLabel("<html>i<br>i<br>i<br>i<br>i<br>i<br>i<br>");
+		JLabel label_2 = new JLabel("");
 		label_2.setBackground(Color.WHITE);
 		label_2.setFont(new Font("Dialog", Font.PLAIN, 15));
 		label_2.setBounds(353, 140, 155, 142);
@@ -99,12 +104,17 @@ public class MemberSelection extends JFrame {
 		if (game.getCrew1().getCrewMemberList().size() >= 3) {
 			CrewMember member3 = game.getCrew1().getCrewMemberList().get(2);
 			label_2.setText(game.convertToMultiline(member3.viewStatus()));
-			JButton btnMember_1 = new JButton(member3.getName());
+			JToggleButton btnMember_1 = new JToggleButton(member3.getName());
 			btnMember_1.setBounds(353, 338, 147, 58);
 			frame.getContentPane().add(btnMember_1);
+			btnMember_1.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					game.setPickedMember(member3);
+				}
+			});
 		}
 		
-		JLabel label_3 = new JLabel("<html>i<br>i<br>i<br>i<br>i<br>i<br>i<br>");
+		JLabel label_3 = new JLabel("");
 		label_3.setForeground(Color.BLACK);
 		label_3.setBackground(Color.WHITE);
 		label_3.setFont(new Font("Dialog", Font.PLAIN, 15));
@@ -113,9 +123,14 @@ public class MemberSelection extends JFrame {
 		if (game.getCrew1().getCrewMemberList().size() >= 4) {
 			CrewMember member4 = game.getCrew1().getCrewMemberList().get(3);
 			label_3.setText(game.convertToMultiline(member4.viewStatus()));
-			JButton btnMember_2 = new JButton(member4.getName());
+			JToggleButton btnMember_2 = new JToggleButton(member4.getName());
 			btnMember_2.setBounds(510, 338, 147, 58);
 			frame.getContentPane().add(btnMember_2);
+			btnMember_2.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					game.setPickedMember(member4);
+				}
+			});
 		}
 
 			
@@ -133,11 +148,21 @@ public class MemberSelection extends JFrame {
 		btnGoBack.setBounds(456, 445, 190, 69);
 		frame.getContentPane().add(btnGoBack);
 		
-
-		
-		
-	
-
+		JButton btnconfirm = new JButton("Confirm");
+		btnconfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (game.getActTaken() == "search") {
+					game.searchPlanet(game.getPickedMember());
+				}
+				goSearchWindow();
+			}
+		});
+		btnconfirm.setBounds(20, 445, 190, 69);
+		frame.getContentPane().add(btnconfirm);
+	}
+	private void goSearchWindow() {
+		game.closeMemberSelection(this);
+		game.launchSearchWindow(); 
 	}
 	private void goBack() {
 		game.closeMemberSelection(this);
@@ -145,7 +170,6 @@ public class MemberSelection extends JFrame {
 	}
 
 	public void closeWindow() {
-		dispose();
+		frame.dispose();
 	}
 }
-	
