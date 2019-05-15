c21f64acfdf11fdefd1111e44f2864d0dbd71ad8
Jerome Grubb <49626010+jfg50@users.noreply.github.com>
2019-05-15 16:34:16 +1200

    Update Crew.java

--- src/game/Crew.java
+++ src/game/Crew.java
@@ -3,7 +3,12 @@ package game;
 
 import java.util.ArrayList;
 
-
+/**
+ * This class contains methods and variables that relate or affect the crew
+ *
+ * @author  
+ * @version 15/05/2019.
+ */
 public class Crew {
     /**
 	 * An arrayList containing all the crew members
@@ -89,54 +94,99 @@ public class Crew {
 	 * money
 	 * days
 	 */
-	
+	/**
+	 * @return 			The number of pieces that the crew have found
+	 */
 	 public int getRequiredPieces() {
 		return requiredPieces;
 	}
+		/**
+		 * @param pieces			the number of pieces that crew have yet to find
+		 */
 	 public void setRequiredPieces(int pieces) {
 	 	this.requiredPieces = pieces;
 	}
+		/**
+		 * @return 			The number of days the crew have been playing
+		 */
 	public int getDays() {
 		return days;
 	}
+	/**
+	 * @param day			The number of days that the crew have been playing
+	 */
 	public void setDays(int day) {
 		this.days = day;
 	}
+	/**
+	 * @param pilotCount			An array list containing all of the crew members ready to fly the ship to a new planet
+	 */
 	public void setPilotCount(ArrayList<CrewMember> pilotCount) {
 		this.pilotCount = pilotCount;
 	}
+	/**
+	 * @return 			The array list of the crew members that are ready to pilot the ship
+	 */
 	public ArrayList<CrewMember> getPilotCount() {
 		return pilotCount;
 	}
+	/**
+	 * @return 			The number of pieces that the crew have found
+	 */
 	public int getNumPieces() {
 		return numPieces;
 	}
+	/**
+	 * @param pieces			the number of pieces the crew has already found
+	 */
 	public void setNumPieces(int pieces) {
 		this.numPieces = pieces;
 	}
+	/**
+	 * @return 			The amount of money that the crew has
+	 */
 	public double getMoney() {
 		return money;
 	}
+	/**
+	 * @param money 			the amount of money the crew has as a double
+	 */
 	public void setMoney(double money) {
 		this.money = money;
 	}
-	
+	/**
+	 * @return 			An array list containing all of the crews medical items
+	 */
 	public ArrayList<MedicalItem> getMedicalItems() {
 		return medicalItems;
 	}
+	/**
+	 * @param medicalItems 			An array list that contains all the crews medical items
+	 */
 	public void setMedicalItems(ArrayList<MedicalItem> medicalItems) {
 		this.medicalItems = medicalItems;
 	}
+	/**
+	 * @return 			An array list containing all of the crews food items
+	 */
 	public ArrayList<Food> getFoodItems() {
 		return foodItems;
 	}
+	/**
+	 * @param foodItems			An array list that contains the crews food items
+	 */
 	public void setFoodItems(ArrayList<Food> foodItems) {
 		this.foodItems = foodItems;
 	}
-	
+	/**
+	 * @return 			An array list containing all of the crew members that are in the crew, and still alive
+	 */
 	public ArrayList<CrewMember> getCrewMemberList() {
 		return crewMemberList;
 	}
+	/**
+	 * @param crewMemberList			An array list containing all of the alive crew members in the crew
+	 */
 	public void setCrewMemberList(ArrayList<CrewMember> crewMemberList) {
 		this.crewMemberList = crewMemberList;
 	}
