6f885c211e485f38f4c2f1ee7d9592db38c073b3
Jerome Grubb <49626010+jfg50@users.noreply.github.com>
2019-05-15 19:53:21 +1200

    Update SpaceShip.java

--- src/game/SpaceShip.java
+++ src/game/SpaceShip.java
@@ -1,33 +1,61 @@
 package game;
-
+/**
+ * This is the class for the space ship, and includes methods and variables that affect the spaceship.
+ *
+ * @author  
+ * @version 15/05/2019.
+ */
 public class SpaceShip {
-	private int shieldHealth = 100;
-	private String shipName;
-	
-	/* Getter and Setter for 
-	 * shieldHealth, shipName
+	/**
+	 * The level of the ships shield
 	 */
-	
-	public int getShieldHealth() {
-		return shieldHealth;
-	}
-
-	public void setShieldHealth(int shieldHealth) {
-		this.shieldHealth = shieldHealth;
-	}
-	public String getShipName(){
-		return shipName;
-	}
-	public void setShipName(String name) {
-		this.shipName = name;
-	}
+	private int shieldHealth = 100;
+	/**
+	 * The ships name
+	 */
+	private String shipName;
+	/**
+	 * repairs the ships shield, while making sure that it does not go over its maximum of 100
+	 * 
+	 * @param repair		How much the ships shield is getting repaired by.		
+	 */
 	public void repairShip(int repair) {
 		setShieldHealth(getShieldHealth() + repair);
 		if (getShieldHealth() > 100) {
 			shieldHealth = 100;
 		}
 	}
+	/**
+	 * Gets the ships shield and name and returns them in a string
+	 * 
+	 * @return		Returns the ships name and shield level in a string
+	 */
 	public String checkShipShield() {
 		return ("\n" + shipName + "'s status:\nShield: " + shieldHealth + "\n");
 	}
+	/**
+	 * @return		The ships shield level
+	 */
+	public int getShieldHealth() {
+		return shieldHealth;
+	}
+	/**
+	 * @param		The ships shield level
+	 */
+	public void setShieldHealth(int shieldHealth) {
+		this.shieldHealth = shieldHealth;
+	}
+	/**
+	 * @return		The ships name
+	 */
+	public String getShipName(){
+		return shipName;
+	}
+	/**
+	 * @param		The ships name
+	 */
+	public void setShipName(String name) {
+		this.shipName = name;
+	}
+
 }
